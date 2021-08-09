66aa68e607fc77be07b18ace8c0c83914b424f9b
ShubhamD21 <dhokashubham681@gmail.com>
2021-08-09 06:40:03 +0000

    New added Pgm

--- first.py
+++ first.py
@@ -1,3 +1,10 @@
 print ("Hello World")
 print ("Hello This is the updated project")
 print ("ShubhAnku is very Smart..!!")
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+ 
+sum = a + b
+ 
+print("sum:", sum)
